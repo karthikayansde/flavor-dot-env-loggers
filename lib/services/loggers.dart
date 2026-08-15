b9f0b62d6516d66cbe931a03ad35a69e9a4a21fe
R. karthi kayan <102909171+karthikayansde@users.noreply.github.com>
2026-08-15 22:30:24 +0530

    Refactor Loggers class for improved method definitions
    Refactored logger methods for conciseness and added optional parameters for error handling.

--- lib/services/loggers.dart
+++ lib/services/loggers.dart
@@ -1,36 +1,31 @@
 import 'package:logger/logger.dart';
 
 class Loggers{
-  static var _logger = Logger(
+  static final _logger = Logger(
     printer: PrettyPrinter(methodCount: 0),
+    printEmojis: true,
   );
   ///Debug // Information useful for debugging
-  static void d(dynamic message){
-    _logger.d(message);
-  }
+  static void d(dynamic message) => _logger.d(message);
 
   ///Trace // Extremely detailed diagnostic info
-  static void t(dynamic message){
-    _logger.t(message);
-  }
+  static void t(dynamic message) => _logger.t(message);
 
   ///Error // Critical failures or exceptions
-  static void e(dynamic message){
-    _logger.e(message);
+  static void e(dynamic message, [dynamic error, StackTrace? stackTrace]){
+    _logger.e(message, error: error, stackTrace: stackTrace);
   }
 
   ///Info // General app flow updates
-  static void i(dynamic message){
-    _logger.i(message);
-  }
+  static void i(dynamic message)  => _logger.i(message);
 
   ///Warning // Potential issues or non-fatal alerts
-  static void w(dynamic message){
-    _logger.w(message);
-  }
+  static void w(dynamic message)  =>  _logger.w(message);
   
   ///Fatal // Total crashes or unrecoverable issues
-  static void w(dynamic message){
-    _logger.w(message);
-  }
+  static void w(dynamic message) => _logger.w(message);
+
 }
+
+// Usage anywhere in your application:
+// AppLogger.info("User successfully logged in.");
